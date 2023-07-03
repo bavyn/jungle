db29d260a6c9b6c0e0cd49fff8836ea794a6ff00
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-07-03 14:18:19 -0700

    admin categories: def new and create

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,25 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_param)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def category_param
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
