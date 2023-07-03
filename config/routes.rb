75f9624709b6deb65ab9a5ce1213ce8b5cff16f8
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-07-03 13:30:35 -0700

    attempt to refactor about route

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   end
 
   get '/about', to: 'about#index'
+  # resource :about, only: [:index]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
