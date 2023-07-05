aa520284c94ca1e0e4da929c77477301e18106ed
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-07-04 18:40:14 -0700

    product specs: requires quantity

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -26,7 +26,17 @@ RSpec.describe Product, type: :model do
       expect(product.errors[:name]).to include("can't be blank")
     end
 
-
+    it 'is invalid when the quantity is empty' do
+      category = Category.create(name: 'Cactus')
+      product = Product.new(
+        name: 'Moon Cactus',
+        price: 1000,
+        quantity: nil,
+        category: category
+      )
+      product.save
+      expect(product.errors[:quantity]).to include("can't be blank")
+    end
 
   end
 end
