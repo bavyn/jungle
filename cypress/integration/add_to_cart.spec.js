b29f90b4fa401a187303a6b186431230cb7db6b3
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-07-05 14:25:05 -0700

    refactor

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,15 +1,9 @@
-describe('Jungle', () => {
+describe('Cart', () => {
   beforeEach(() => {
     cy.visit('/')
+    cy.get(".products article").should("be.visible")
+    cy.get(".products article").should("have.length", 2)
   })
-
-  it("shows products on the page", () => {
-    cy.get(".products article").should("be.visible");
-  });
-
-  it("shows 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
-  });
 
   it("should have an empty cart", () => {
     cy.get('a[href*="cart"]').contains('0')
--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,20 +1,12 @@
-describe('Jungle', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
-  it("shows products on the page", () => {
-    cy.get(".products article").should("be.visible");
-  });
-
-  it("shows 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
-  });
-
+describe('Product Details', () => {
   it('navigates from the home page to the product detail page when a product is clicked', () => {
+    cy.visit('/')
+    cy.get(".products article").should("be.visible")
+    cy.get(".products article").should("have.length", 2)
+
     cy.get(".products article")
       .first()
       .click()
-    cy.get(".product-detail").should("be.visible");
+    cy.get(".product-detail").should("be.visible")
   });
 })
