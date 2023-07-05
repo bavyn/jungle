88373b9b4bfe392970e76dd5c495d73c787d39e9
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-07-05 03:42:11 -0700

    cypress home spec first test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,10 @@
+describe('Jungle', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products > article").should("be.visible");
+  });
+
+})
