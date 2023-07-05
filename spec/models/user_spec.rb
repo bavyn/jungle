e6a0ae1a20ea10050651342d42ddc286e6658369
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-07-04 20:30:05 -0700

    user specs: require first and last names

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'is invalid when the first name is empty' do
+      user = User.new(first_name: nil).save
+      expect(user).to eq(false)
+    end
+
+    it 'is invalid when the last name is empty' do
+      user = User.new(last_name: nil).save
+      expect(user).to eq(false)
+    end
+
+
+  end
 end
