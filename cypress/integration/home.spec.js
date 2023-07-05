c37e1704e54e8b4291d39c7fba1465102ca65a38
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-07-05 12:39:18 -0700

    feature spec home page complete

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,10 +1,13 @@
 describe('Jungle', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
   })
 
-  it("There is products on the page", () => {
-    cy.get(".products > article").should("be.visible");
+  it("shows products on the page", () => {
+    cy.get(".products article").should("be.visible");
   });
 
+  it("shows 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 })
